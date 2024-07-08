fe2ce25ec0ddf23c21f1eb4697dfe4752bd373f8
silchenko-arsen <silchenkoarsen@gmail.com>
2024-07-08 19:27:59 +0300

    add indexes

--- task.sql
+++ task.sql
@@ -1,2 +1,4 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+# Write your SQL code for the database creation here. Good luck!
+USE ShopDB;
+CREATE INDEX Email ON Customers(Email);
+CREATE INDEX Name ON Products(Name);
